86465cd613fefd7e7264f2b9ebea464316799191
Ondra Jodas <ondrej.jodas@keboola.com>
2022-01-21 17:22:46 +0100

    increase number of retries

--- src/Storages/AzureBlobStorage.php
+++ src/Storages/AzureBlobStorage.php
@@ -20,7 +20,7 @@ class AzureBlobStorage implements IStorage
 
     public const SAS_DEFAULT_EXPIRATION_HOURS = 36;
 
-    private const MAX_RETRIES = 5;
+    private const MAX_RETRIES = 20;
 
     public function __construct(Config $config, LoggerInterface $logger)
     {
@@ -34,7 +34,10 @@ class AzureBlobStorage implements IStorage
         $absClient->pushMiddleware(
             RetryMiddlewareFactory::create(
                 RetryMiddlewareFactory::GENERAL_RETRY_TYPE,
-                self::MAX_RETRIES
+                self::MAX_RETRIES,
+                3000,
+                RetryMiddlewareFactory::EXPONENTIAL_INTERVAL_ACCUMULATION,
+                true
             )
         );
 
